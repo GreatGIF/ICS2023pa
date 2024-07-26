cc797acd60e7f97e7a438af8e30d4ecf3fd4a71c
xh <xh13242981739@qq.com>
2024-07-26 09:27:47 +0800

    pa3.2 file_test

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -9,7 +9,10 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t fp_idx;
 } Finfo;
+
+static int file_num = 0;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
@@ -23,14 +26,68 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+size_t serial_write(const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+  file_num = sizeof(file_table) / sizeof(Finfo);
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for (int i = 0; i < file_num; i++) {
+    if(strcmp(pathname, file_table[i].name) == 0) {
+      file_table[i].fp_idx = 0;
+      return i;
+    }
+  }
+  assert(0);
+}
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t fs_read(int fd, void *buf, size_t len) {
+  if(file_table[fd].read) {
+    return file_table[fd].read(buf, file_table[fd].fp_idx, len);
+  }
+  size_t max_len = file_table[fd].size - file_table[fd].fp_idx;
+  len = len > max_len ? max_len : len;
+  size_t ret = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].fp_idx, len);
+  file_table[fd].fp_idx += ret;
+  return ret;
+}
+
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len) {
+  if(file_table[fd].write) {
+    return file_table[fd].write(buf, file_table[fd].fp_idx, len);
+  }
+  size_t max_len = file_table[fd].size - file_table[fd].fp_idx;
+  len = len > max_len ? max_len : len;
+  size_t ret = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].fp_idx, len);
+  file_table[fd].fp_idx += ret;
+  return ret;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  switch(whence) {
+  case SEEK_CUR: break;
+  case SEEK_END: file_table[fd].fp_idx = file_table[fd].size; break;
+  case SEEK_SET: file_table[fd].fp_idx = 0; break;
+  default: return -1;
+  }
+  file_table[fd].fp_idx += offset;
+  if(file_table[fd].fp_idx > file_table[fd].size) {
+    file_table[fd].fp_idx = file_table[fd].size;
+  }
+  return 0;
+}
+int fs_close(int fd) {
+  file_table[fd].fp_idx = 0;
+  return 0;
 }
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -25,16 +26,23 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr elf_header;
-  ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
+  int fp = fs_open(filename, 0, 0);
+  fs_read(fp, &elf_header, sizeof(Elf_Ehdr));
+  // printf("fp:%d\n", fp);
+  // ramdisk_read(&elf_header, 0, sizeof(Elf_Ehdr));
 
   assert(*(uint32_t *)elf_header.e_ident == 0x464c457f);
   assert(elf_header.e_machine == EXPECT_TYPE);
 
   Elf_Phdr program_header_tb[elf_header.e_phnum];
-  ramdisk_read(program_header_tb, elf_header.e_phoff, elf_header.e_phnum * elf_header.e_phentsize);
+  fs_lseek(fp, elf_header.e_phoff, SEEK_SET);
+  fs_read(fp, program_header_tb, elf_header.e_phnum * elf_header.e_phentsize);
+  // ramdisk_read(program_header_tb, elf_header.e_phoff, elf_header.e_phnum * elf_header.e_phentsize);
   for(int i = 0; i < elf_header.e_phnum; i++) {
     if(program_header_tb[i].p_type != PT_LOAD) {continue;}
-    ramdisk_read((void *)program_header_tb[i].p_vaddr, program_header_tb[i].p_offset, program_header_tb[i].p_filesz);
+    fs_lseek(fp, program_header_tb[i].p_offset, SEEK_SET);
+    fs_read(fp, (void *)program_header_tb[i].p_vaddr, program_header_tb[i].p_filesz);
+    // ramdisk_read((void *)program_header_tb[i].p_vaddr, program_header_tb[i].p_offset, program_header_tb[i].p_filesz);
     memset((void *)(program_header_tb[i].p_paddr + program_header_tb[i].p_filesz), 0, program_header_tb[i].p_memsz - program_header_tb[i].p_filesz);
   }
   return elf_header.e_entry;
