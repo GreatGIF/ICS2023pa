e08562da1fe0ca16376d712702df3249e8cdf4c3
xh <xh13242981739@qq.com>
2024-08-14 00:04:07 +0800

    finish execve

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -28,70 +28,78 @@ static void sh_prompt() {
   sh_printf("sh> ");
 }
 
-static int cmd_help(char *args);
-static int cmd_echo(char *args);
+// static int cmd_help(char *args);
+// static int cmd_echo(char *args);
 
-static struct {
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "echo", "print to cmd", cmd_echo },
-};
+// static struct {
+//   const char *name;
+//   const char *description;
+//   int (*handler) (char *);
+// } cmd_table [] = {
+//   { "help", "Display information about all supported commands", cmd_help },
+//   { "echo", "print to cmd", cmd_echo },
+// };
 
-static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
+// static int cmd_help(char *args) {
+//   /* extract the first argument */
+//   char *arg = strtok(NULL, " ");
+//   int i;
 
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      sh_printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-    }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
-    }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
-}
+//   if (arg == NULL) {
+//     /* no argument given */
+//     for (i = 0; i < NR_CMD; i ++) {
+//       sh_printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+//     }
+//   }
+//   else {
+//     for (i = 0; i < NR_CMD; i ++) {
+//       if (strcmp(arg, cmd_table[i].name) == 0) {
+//         printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+//         return 0;
+//       }
+//     }
+//     printf("Unknown command '%s'\n", arg);
+//   }
+//   return 0;
+// }
 
-// extern uint32_t expr(char *e, bool *success);
-static int cmd_echo(char *args) {
-  char *arg = strtok(NULL, "\n");
-  bool success;
-  printf("%s\n", arg);
-  // int ret = expr(args, &success);
-  // printf("%d", ret);
-  return 0;
-}
+// // extern uint32_t expr(char *e, bool *success);
+// static int cmd_echo(char *args) {
+//   char *arg = strtok(NULL, "\n");
+//   bool success;
+//   printf("%s\n", arg);
+//   // int ret = expr(args, &success);
+//   // printf("%d", ret);
+//   return 0;
+// }
 
 static void sh_handle_cmd(const char *cmd) {
-  char *temp = strtok((char *)cmd, " ");
-  printf("111%s\n", temp);
-  if (temp == NULL) { return; }
-  int i;
-  for (i = 0; i < NR_CMD; i ++) {
-    if (strcmp(cmd, cmd_table[i].name) == 0) {
-      if (cmd_table[i].handler(temp) < 0) { return; }
-      break;
-    }
-  }
+  // char *temp = strtok((char *)cmd, " ");
+  // if (temp == NULL) { return; }
+  // int i;
+  // for (i = 0; i < NR_CMD; i ++) {
+  //   if (strcmp(cmd, cmd_table[i].name) == 0) {
+  //     if (cmd_table[i].handler(temp) < 0) { return; }
+  //     break;
+  //   }
+  // }
 
-    if (i == NR_CMD) { printf("Unknown command '%s'\n", temp); }
+  // if (i == NR_CMD) { printf("Unknown command '%s'\n", temp); }
+  int len = strlen(cmd);
+  char *str = (char *)malloc(len);
+  strncpy(str, cmd, len - 1);
+  str[len - 1] = 0;
+  execvp(str, NULL);
 }
 
 void builtin_sh_run() {
   sh_banner();
+  if (setenv("PATH", "/bin", 0) == 0) {
+    sh_printf("PATH set to: %s\n", "/bin");
+  } else {
+    perror("setenv");
+  }
   sh_prompt();
-
   while (1) {
     SDL_Event ev;
     if (SDL_PollEvent(&ev)) {
